d6beffb65bf4e0e5f2276869e91f455893fd0785
Mathieu Filotto <filotto.mathieu@gmail.com>
2023-06-27 05:30:56 +1100

    feat(install.sh): adapt to lima dev env

--- install.sh
+++ install.sh
@@ -13,51 +13,12 @@ then
     exit 1
 fi
 
-echo "Check if environment variables are set"
-if [ -z "$ARTIFACTORY_URL" ]
-then
-    echo "ARTIFACTORY_URL environment variable must be set in Windows with WSLENV to propagate"
-    exit 1
-fi
-
-if [ -z "$ARTIFACTORY_USER" ]
-then
-    echo "ARTIFACTORY_USER environment variable must be set in Windows with WSLENV to propagate"
-    exit 1
-fi
-
-if [ -z "$ARTIFACTORY_PASSWORD" ]
-then
-    echo "ARTIFACTORY_PASSWORD environment variable must be set in Windows with WSLENV to propagate"
-    exit 1
-fi
-
-ARTIFACTORY_FQDN=${ARTIFACTORY_URL##*/}
-
-echo "Add Artifactory apt authentication file"
-cat << EOF | sudo tee /etc/apt/auth.conf.d/artifactory-auth > /dev/null
-machine ${ARTIFACTORY_FQDN}
-  login ${ARTIFACTORY_USER}
-  password ${ARTIFACTORY_PASSWORD}
-EOF
-
-echo "Create Artifactory apt sources file"
-if [ ! -f /etc/apt/sources.list.public ]
-then
-    echo "Backup original apt sources file"
-    sudo cp /etc/apt/sources.list /etc/apt/sources.list.public
-fi
-sudo cp /etc/apt/sources.list /etc/apt/sources.list.artifactory
-sudo sed -i "s/deb http:\/\/archive.ubuntu.com\/ubuntu/deb [trusted=yes] https:\/\/$ARTIFACTORY_FQDN\/artifactory\/debian-archive-ubuntu-remote/g" /etc/apt/sources.list.artifactory
-sudo sed -i "s/deb http:\/\/security.ubuntu.com\/ubuntu/deb [trusted=yes] https:\/\/$ARTIFACTORY_FQDN\/artifactory\/debian-security-ubuntu-remote/g" /etc/apt/sources.list.artifactory
-sudo cp /etc/apt/sources.list.artifactory /etc/apt/sources.list
-
 echo "Add Saltstack apt source file"
 UBUNTU_CODENAME=`lsb_release -cs`
 UBUNTU_RELEASE=`lsb_release -rs`
-cat << EOF | sudo tee /etc/apt/sources.list.d/saltstack.list > /dev/null
-deb [trusted=yes] https://${ARTIFACTORY_FQDN}/artifactory/debian-saltstack-ubuntu-remote/py3/ubuntu/${UBUNTU_RELEASE}/amd64/latest ${UBUNTU_CODENAME} main
-EOF
+sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/ubuntu/$UBUNTU_RELEASE/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
+echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/$UBUNTU_RELEASE/amd64/latest $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/salt.list
+
 
 echo "Update packages"
 sudo apt update
@@ -70,10 +31,13 @@ cat << EOF | sudo tee /etc/salt/minion.d/masterless.conf > /dev/null
 file_client: local
 file_roots:
   base:
-    - /srv/wsl/salt
+    - /srv/lima/salt
 pillar_roots:
   base:
-    - /srv/wsl/pillar
+    - /srv/lima/pillar
 EOF
+
+echo "Install dev env"
+sudo salt-call --local state.highstate
 
 echo "salt command example: sudo salt-call --local grains.items"
