79840419d661dd7cadf1ed4c6a23810aa0962340
Mathieu Filotto <filotto.mathieu@gmail.com>
2023-06-27 16:22:24 +1100

    feat(install.sh): install everything with one command

--- install.sh
+++ install.sh
@@ -38,33 +38,53 @@ then
     exit 1
 fi
 
+WORKSPACE="/tmp/lima/workspace"
+printmainstep "Install lima with brew"
+brew install lima
+
+printmainstep "Create workspace folder $WORKSPACE"
+mkdir -p $WORKSPACE
+
+printmainstep "Create your distro"
+limactl start --name=default template://ubuntu-lts --tty=false
+
+printmainstep "Start your distro"
+limactl start
+
+printmainstep "Clone lima dev env from Github repo"
+lima sh -c 'cd $WORKSPACE && sudo git clone https://github.com/mfilotto/lima-dev-env.git`'
+
 printmainstep "Add Saltstack apt source file"
 UBUNTU_CODENAME=`lsb_release -cs`
 UBUNTU_RELEASE=`lsb_release -rs`
-sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/ubuntu/$UBUNTU_RELEASE/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg
-echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/$UBUNTU_RELEASE/amd64/latest $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/salt.list
+lima sh -c 'sudo curl -fsSL -o /etc/apt/keyrings/salt-archive-keyring-2023.gpg https://repo.saltproject.io/salt/py3/ubuntu/$UBUNTU_RELEASE/amd64/SALT-PROJECT-GPG-PUBKEY-2023.gpg'
+lima sh -c 'echo "deb [signed-by=/etc/apt/keyrings/salt-archive-keyring-2023.gpg arch=amd64] https://repo.saltproject.io/salt/py3/ubuntu/$UBUNTU_RELEASE/amd64/latest $UBUNTU_CODENAME main" | sudo tee /etc/apt/sources.list.d/salt.list'
 
 printmainstep "Update packages"
-sudo apt update
+lima sudo apt update
 
 printmainstep "Install salt minion"
-sudo apt install -y salt-minion
+lima sudo apt install -y salt-minion
 
 printmainstep "Configure salt minion"
-cat << EOF | sudo tee /etc/salt/minion.d/masterless.conf > /dev/null
+lima sh -c 'cat << EOF | sudo tee /etc/salt/minion.d/masterless.conf > /dev/null
 file_client: local
 file_roots:
   base:
-    - /srv/lima/salt
+    - $WORKSPACE/lima-dev-env/salt
 pillar_roots:
   base:
-    - /srv/lima/pillar
-EOF
+    - $WORKSPACE/lima-dev-env/pillar
+EOF'
 
 printmainstep "Install lima dev env with salt"
-sudo salt-call --local state.highstate
+lima sudo salt-call --local state.highstate
 
 printmainstep "Source .bashrc"
-source $HOME/.bashrc
+lima source $HOME/.bashrc
+lima source /etc/profile.d/*
+
+printmainstep "Enter inside lima vm "
+lima
 
 echo "salt command example: sudo salt-call --local grains.items"
